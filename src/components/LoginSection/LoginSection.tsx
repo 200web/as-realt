a47328b1aeff02837f73df6291edf81c712cddbd
200web <200webstudio@gmail.com>
2025-05-09 14:21:00 +0200

    login page

--- src/components/LoginSection/LoginSection.tsx
+++ src/components/LoginSection/LoginSection.tsx
@@ -7,9 +7,9 @@ export default function LoginSection() {
   return (
     <section className={styles.loginSection}>
       <div className={styles.container}>
-        <h2 className={styles.title}>Вход в личный кабинет продавца</h2>
+        <h2 className={styles.title}>Вход в личный кабинет</h2>
         <form className={styles.form}>
-          <input type="Name" placeholder="Ваше имя" className={styles.input} />
+          <input type="Name" placeholder="Номер телефона" className={styles.input} />
           <input
             type="password"
             placeholder="Пароль"
